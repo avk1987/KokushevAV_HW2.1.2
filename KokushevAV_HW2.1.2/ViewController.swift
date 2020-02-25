7cbb408d859301e8aed3e04635e1b88117fd4129
AVK <kokushev@yandex.ru>
2020-02-25 23:16:39 +0500

    Ending

--- KokushevAV_HW2.1.2/ViewController.swift
+++ KokushevAV_HW2.1.2/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.setTitle("Start", for: .normal)
+        redLight.layer.cornerRadius = redLight.frame.size.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
+        
     }
 
-
+    
+    @IBAction func startBurronPressed() {
+        
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else if greenLight.alpha == 1 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        } else {
+            redLight.alpha = 1
+            startButton.setTitle("Next", for: .normal)
+        }
+    }
+    
 }
 
